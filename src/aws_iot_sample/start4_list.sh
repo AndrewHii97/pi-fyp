bd830deee556049c24824de811a43ba828d5b293
AndrewHii97 <hiichiileong@gmail.com>
2021-03-21 13:10:23 +0800

    master: Remove Sample source code from the directory
    - sample source code is saved into other directory in laptop

--- src/aws_iot_sample/start4_list.sh
+++
@@ -1,3 +0,0 @@
-set -e 
-printf "\nRunning application...\n"
-python3 aws-iot-device-sdk-python/samples/basicShadow/basicShadowDeltaListener.py -e a3iz7l1satteal-ats.iot.us-east-1.amazonaws.com  -r root-CA.crt -c my_raspi.cert.pem -k my_raspi.private.key
