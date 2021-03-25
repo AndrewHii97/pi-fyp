07d263b35207d692308425abfc7a9160e5c37426
AndrewHii97 <hiichiileong@gmail.com>
2021-03-25 22:50:07 +0800

    dev: Add capture photo and upload on motion sensor trigger

--- src/fyp/fyp.py
+++ src/fyp/fyp.py
@@ -1,4 +1,5 @@
 import sys
+import uuid
 from iot_core import IoTShadow 
 import logging
 import threading 
@@ -27,9 +28,11 @@ def main(args):
 	logger.info("gate switch initialized")
 	glock = device.DoorLock(35,37)
 	logger.info("door lock initialized")
-	usonic = device.USonicSensor(10,8)
-	logger.info("ultrasonic sensor initialized")
-	client = Client(DEVICE_NAME, PASSWORD) 
+	motion_sensor = device.MotionSensor(8)
+	logger.info("motion sensor initialized")
+	camera = device.Camera()
+	logger.info("camera initialized")
+	client = Client('http://192.168.0.137:3000',DEVICE_NAME, PASSWORD) 
 	logger.info("Create HTTP Client to handle requests to server") 
 	# rfid wait for rfid scan 
 	rfid.read()
@@ -37,48 +40,52 @@ def main(args):
 	r = client.check_rfid(rfid.get_id())
 	res = r.json()
 	logger.debug(f'Response:{res}') 
+
 	if res['status'] == True and len(res['keyowner']): 
 		for counter in range(100):
 			# open the gate lock 
 			motor.backward(0.001)
+
 	logger.debug('wait for gate to be open') 
 	while glock.get_status() == 1: 
-		pass
+		time.sleep(0.1)
 		
-	# lock the gate when the door return to original position
-	def lockdoor_thread(): 
+	def lockdoor_thread():  # thread lock gate upon gate closed
 		while glock.get_status() == 0 : 
 			time.sleep(0.1)
-		logger.info("door return to orignial position")
-		logger.info("motor close lock the door ")
+		logger.info("door return to original position")
+		logger.info("motor rotate to close the door")
 		for counter in range(100): 
 			time.sleep(0.001)
 			motor.forward(0.001)
-		logger.info("lock door thread completed")
+		logger.info("door is locked")
 		
 	time.sleep(2)	
 	logger.info("lock door thread started")
 	lock_door = threading.Thread(target=lockdoor_thread)
 	lock_door.start()
-	logger.debug(f'Start monitoring Distance')
-	while True: 
-		print(usonic.get_distance())
-	logger.info("someone detected") 
-	
-	
-	# wait for the door to close 
-	logger.info("wait for door to close")
+	logger.info("start sensing for motion")	
+	while motion_sensor == 0 :
+		time.sleep(1)
+		logger.debug("no person") 
+	logger.info("motion sensor detect person")
+	byte = camera.capture() # capture images after motion sensor trigger 
+	file_name = str(uuid.uuid1()) # generate uuid from hostname and time 
+	logger.debug("UUID:{file_name}")
+	r = client.upload_images(file_name,byte)
+	logger.debug(r)
+
 	lock_door.join()
 	logger.info("main completed")
 	return 0
 	# motor will move to open the gate at this moment if rfid valid  
 	# while glock.get_status() == True : 
-	# 	print("motor moving",end="\r")
-	# 	motor.forward(0.2) 
+	#	print("motor moving",end="\r")
+	#	motor.forward(0.2) 
 	# usonic.calibrate()
 	# # ultrasonic sensor try to detect if any perons run through 
 	# while usonic.get_distance() > 20: 
-	# 	print("no person detected yet") 
+	#	print("no person detected yet") 
 	# print("usonicsensor detect something") 
 	# camera turn on to capture the scene
 	# the captured picture is sent to the cloud 
